75b01f221077a73a551228aea6e977a310d1bac2
Flor <florencgmz@gmail.com>
2023-01-23 21:08:57 -0300

    add source code link

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
         banner='banner02' />
         <Download/>
         <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
-          <p className={`${styles.pText} ${styles.whiteText}`}>Made by love by Plantasia</p>
+          <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by Plantasia</p>
         </div>
     </>
   );
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -9,7 +9,9 @@ const Download = () => {
         <div>
           <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
           <p className={`${styles.pText} ${styles.blackText}`}>Get the full Source Code on Github</p>
-          <button className={styles.btnPrimary}>Source code</button>
+          <a href='https://github.com/1terabyteflor/showcase' className={`${styles.btnPrimary} mt-4`}>
+            Source Code
+          </a>
           <img 
             src={assets.scene}
             alt="download_png"
